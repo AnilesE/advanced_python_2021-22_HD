4503ac5daf151d86449e150de44af79e6460b8c7
Selina <sernst2001@gmail.com>
2021-11-15 14:29:48 +0100

    load files

---
+++ peak_finder/basic.py
@@ -0,0 +1,41 @@
+#
+# The first version of our function!
+# Write doc strings 
+#
+def find_peaks(list_of_intensities):
+    """Find peaks
+
+    Find local maxima for a given list of intensities or tuples
+    Intensities are defined as local maxima if the
+    intensities of the elements in the list before and after
+    are smaller than the peak we want to determine.
+
+    For example given a list:
+        1 5 [6] 4 1 2 [3] 2
+
+    We expect 6 and 3 to be returned.
+    
+
+    Args:
+        list_of_intensities (list of floats, ints or tuple of ints): a list of
+            numeric values
+
+    Returns:
+        list of floats or tuples: list of the identified local maxima
+
+    Note:
+        This is just a place holder for the TDD part :)
+
+    """
+    peaks = []
+    for i in range(len(list_of_intensities)):
+        if i==0:
+            if list_of_intensities[i] > list_of_intensities[i+1]: peaks.append(list_of_intensities[i])
+        elif i != 0 and i != len(list_of_intensities):
+            if list_of_intensities[i-1] < list_of_intensities[i] > list_of_intensities[i+1]:
+                peaks.append(list_of_intensities[i])
+        elif i==len(list_of_intensities):
+            if list_of_intensities[i] > list_of_intensities[i+1]: peaks.append(list_of_intensities[i])
+
+
+    return peaks
